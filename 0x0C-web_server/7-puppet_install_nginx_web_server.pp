34d8257785712c8033c04c65313290ba290740d1
Matthew Zakari <zakarimatthew62@gmail.com>
2024-05-08 19:44:11 -0700

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,22 +6,29 @@ package { 'nginx':
 }
 
 # Allow HTTP traffic for Nginx
-firewall { '100 Allow nginx HTTP':
-  port   => 80,
-  proto  => 'tcp',
-  action => 'accept',
-}
+#firewall { '100 Allow nginx HTTP':
+#  port   => 80,
+#  proto  => 'tcp',
+#  action => 'accept',
+#}
 
 # Define the Nginx server configuration
-file_line { 'install':
+#file_line { 'install':
+#  ensure  => 'present',
+#  path    => '/etc/nginx/sites-available/default',
+#  line    => 'error_page 404 /404.html;',
+#}
+
+file_line { 'activate':
   ensure  => 'present',
-  path	  => '/etc/nginx/sites-enabled/default',
-  after   => 'listen 80 default_server',
-  line    => 'rewrite ^/redirect_me https://github.com/MatthewZakari permanent',
+  path    => '/etc/nginx/sites-enabled/default',
+  line    => 'rewrite ^/redirect_me https://github.com/MatthewZakari permanent;',
+  require => File_line['install'],
 }
 
 # Define the index.html file
 file { '/var/www/html/index.html':
+  ensure  => 'present',
   content => 'Hello World!',
 }
 
